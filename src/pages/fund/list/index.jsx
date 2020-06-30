9951cd222f2c09dc459449a0798afbabb19fa6aa
wt <wangtong0216@outlook.com>
2020-06-30 20:05:33 +0800

    fund查询

--- src/pages/fund/list/index.jsx
+++ src/pages/fund/list/index.jsx
@@ -1,228 +1,62 @@
 import React, { useState, useEffect } from 'react'
-import { Table, Input, Popconfirm, Form, PageHeader, Space } from 'antd'
-import { CirclePicker } from 'react-color'
-import dayjs from 'dayjs'
+import { Table, PageHeader } from 'antd'
+import { withRouter } from 'react-router-dom'
 import axios from 'axios'
-
-const EditableCell = ({
-    editing,
-    dataIndex,
-    title,
-    inputType,
-    record,
-    children,
-    setColor,
-    color,
-    ...restProps
-}) => {
-
-    const handleChangeColor = (color) => {
-        setColor(color.hex)
-    }
-    const inputNode = inputType === 'color' ? <CirclePicker color={color} onChange={handleChangeColor} /> : <Input />
-    return (
-        <td {...restProps}>
-            {editing ? (
-                <Form.Item
-                    name={dataIndex}
-                    style={{
-                        margin: 0,
-                    }}
-                    rules={[
-                        {
-                            required: true,
-                            message: `请输入${title}!`,
-                        },
-                    ]}
-                >
-                    {inputNode}
-                </Form.Item>
-            ) : (
-                    children
-                )}
-        </td>
-    );
-};
-
-const FundList = () => {
-
-    const [form] = Form.useForm();
-    const [data, setData] = useState([]);
-    const [editingKey, setEditingKey] = useState('');
-    const [color, setColor] = useState('')
-    const [updateFlg, setUpdateFlg] = useState(false)
-    const isEditing = record => record._id === editingKey;
-
-    useEffect(() => {
-        axios.get('/api/getAllTags').then(
-            (response) => {
-                let tags = response.data.res
-                setData(tags)
-            },
-            ({ response }) => {
-                console.log(response)
-            }
-        )
-    }, [editingKey, color, updateFlg])
-
-    const edit = record => {
-        form.setFieldsValue({
-            ...record,
-        })
-        setEditingKey(record._id)
-        setColor(record.color)
-    }
-
-    const cancel = () => {
-        setEditingKey('')
-    }
-
-    const save = async record => {
-        let row = await form.validateFields();
-        console.log(row)
-        let request_body = {
-            name: row.name,
-            color: color,
-            update_dt: Date.now()
-        }
-        try {
-            axios.post(`/api/updateTag/${record._id}`, request_body).then(
-                () => {
-                    setEditingKey('')
-                }
-            )
-        } catch (error) {
-            console.log('Validate Failed:', error);
-        }
-    }
-
-    const deleteTag = async id => {
-        console.log(id)
-        try {
-            axios.get(`/api/deleteTag/${id}`).then(
-                () => {
-                    setUpdateFlg(!updateFlg)
-                }
-            )
-        } catch (error) {
-
-        }
-    }
-
+const FundList = (props) => {
     const columns = [
         {
-            title: '标签名称',
+            title: 'No.',
+            key: 'no',
+            width: 20,
+            render: (text, record, dataIndex) => <span>{dataIndex + 1}</span>
+        },
+        {
+            title: 'serialNumber',
+            dataIndex: 'serialNumber',
+            key: 'serialNumber',
+            width: 300
+        },
+        {
+            title: 'fundName',
             dataIndex: 'name',
             key: 'name',
-            width: '25%',
-            editable: true,
+            width: 300
         },
         {
-            title: '标签颜色',
-            dataIndex: 'color',
-            key: 'color',
-            width: '10%',
-            editable: true,
-            render: (text) =>
-                <div
-                    tabIndex="0"
-                    style={{
-                        background: 'transparent',
-                        height: '28px',
-                        width: '28px',
-                        cursor: 'pointer',
-                        position: 'relative',
-                        outline: 'none',
-                        'borderRadius': '50%',
-                        'boxShadow': text + ' 0px 0px 0px 15px inset',
-                        transition: 'box-shadow 100ms ease 0s'
-                    }}>
-                </div>
+            title: 'position',
+            dataIndex: 'position',
+            key: 'position',
+            width: 300
         },
         {
-            title: '更新时间',
-            dataIndex: 'update_dt',
-            key: 'update_dt',
-            width: '15%',
-            render: (text) => <div>{dayjs(text).format('YYYY-MM-DD HH:mm:ss')}</div>
-        },
-        {
-            title: 'action',
-            dataIndex: 'action',
-            key: 'action',
-            render: (_, record) => {
-                const editable = isEditing(record);
-                return editable ? (
-                    <span>
-                        <Space size="middle">
-                            <a
-                                onClick={() => save(record)}
-                                style={{
-                                    marginRight: 8,
-                                }}
-                            >
-                                保存
-                            </a>
-                            <a onClick={cancel}>
-                                取消
-                            </a>
-                        </Space>
-                    </span>
-                ) : (
-                        <span>
-                            <Space size="middle">
-                                <a disabled={editingKey !== ''} onClick={() => edit(record)}>
-                                    修改
-                                </a>
-                                <Popconfirm title="确认删除?" onConfirm={() => deleteTag(record._id)}>
-                                    <a>删除</a>
-                                </Popconfirm>
-                            </Space>
-                        </span>
-                    )
-            },
-        },
-    ];
-    const mergedColumns = columns.map(col => {
-        if (!col.editable) {
-            return {
-                ...col
+            title: 'applies',
+            dataIndex: 'applies',
+            key: 'applies',
+            width: 300
+        }
+    ]
+
+    const [fund, setFund] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+
+        setLoading(true)
+
+        axios.get('/api/getAllFund').then(
+            (res) => {
+                console.log(res)
+                setFund(res.data.res.fundList)
             }
-        }
-
-        return {
-            ...col,
-            onCell: record => ({
-                record,
-                color,
-                setColor,
-                inputType: col.dataIndex === 'color' ? 'color' : 'text',
-                dataIndex: col.dataIndex,
-                title: col.title,
-                editing: isEditing(record)
-            }),
-        }
-    })
-
+        )
+        setLoading(false)
+    }, [loading])
     return (
-        <Form form={form} component={false}>
-            <PageHeader className="site-page-header" title="标签列表" />
-            <Table
-                components={{
-                    body: {
-                        cell: EditableCell,
-                    },
-                }}
-                bordered
-                dataSource={data}
-                columns={mergedColumns}
-                rowClassName="editable-row"
-                pagination={{
-                    onChange: cancel,
-                }}
-                rowKey={record => record._id}
-            />
-        </Form>
+        <section>
+            <PageHeader className="site-page-header" title="fund列表" />
+            <Table columns={columns} rowKey={record => record.serialNumber} dataSource={fund} bordered='true' />
+        </section>
     )
 }
-export default FundList
+
+export default withRouter(FundList)
