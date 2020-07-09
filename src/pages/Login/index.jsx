ee44878998e61a161817291f4d29af5760c970fd
wt-maker <52414106+wt-maker@users.noreply.github.com>
2020-07-09 16:58:36 +0800

    登录画面

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Layout, Row, Col, Input, Form, Button } from 'antd'
+import { UserOutlined, LockOutlined, DownloadOutlined } from '@ant-design/icons';
+import './login.scss'
+const LoginPage = (props) => {
+
+    const onFinish = (values) => {
+        console.log(values)
+    }
+    return (
+        <Form onFinish={onFinish} layout='horizontal' className="addTag">
+            <Layout id="login-page">
+                <Row>
+                    <Col span={9}>
+                        <div id="login-logo">
+                            <a href="/">
+                                博客后台管理
+                        </a>
+                        </div>
+                    </Col>
+                    <Col span={6}>
+                        <div id="login-form">
+                            <h4 className="title">
+                                <div id="login-title">
+                                    <a>登录</a>
+                                    {/*  <b>·</b>
+                                <a>注册</a> */}
+                                </div>
+                            </h4>
+                            <Form.Item
+                                name='username'
+                                rules={[{ required: true, message: '请输入用户名' }]}
+                            >
+                                <Input prefix={<UserOutlined />} placeholder="用户名" />
+                            </Form.Item>
+                            <Form.Item
+                                name='password'
+                                rules={[{ required: true, message: '请输入密码' }]}
+                            >
+                                <Input.Password prefix={<LockOutlined />} placeholder="请输入密码" />
+                            </Form.Item>
+                            <Button type="primary" shape="round" id="login-button" htmlType='submit'>
+                                登录
+                        </Button>
+                        </div>
+                    </Col>
+                    <Col span={9}></Col>
+                </Row>
+            </Layout>
+        </Form>
+    )
+}
+
+export default LoginPage
